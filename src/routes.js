30c6f4b4222c2518d2cbd7940cc2792c0380907f
Guilherme Vanderley <guivdouradog@gmail.com>
2022-08-31 23:11:03 -0300

    created dashboard

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from './config/upload';
 
+import DashboardController from './controllers/DashboardController';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 
@@ -13,5 +14,7 @@ routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
+
+routes.get('/dashboard', DashboardController.show);
 
 export default routes;
